8ac171a56ed0db9e70cc1db0f79c0678637b9928
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-10-28 15:18:19 -0300

    criado método de editar (corrigido o método de listar by id)

--- app.py
+++ app.py
@@ -24,10 +24,18 @@ books = [
 def get_books():
     return jsonify(books)
 
-@app.route('/book/<int:id>', methods=['GET'])
+@app.route('/books/<int:id>', methods=['GET'])
 def get_book_by_id(id):
     for book in books:
         if book.get('id') == id:
-            return jsonify(books)
+            return jsonify(book)
+
+@app.route('/books/<int:id>', methods=['PUT'])
+def update_book_by_id(id):
+    updated_book = request.get_json()
+    for index, book in enumerate(books):
+        if book.get('id') == id:
+            books[index].update(updated_book)
+            return jsonify(books[index])
 
 app.run(port=5000, host='localhost', debug=True)
