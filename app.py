a78ff35e9c02c0ebbca66496a2f94e3d8b7446da
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-10-29 00:49:42 -0300

    crud completo e vinculado com o banco de dados (proximo passo: tratativa de erros)

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request
 import mysql.connector
-import json
 
 connection = mysql.connector.connect(
     host = 'localhost',
@@ -9,55 +8,32 @@ connection = mysql.connector.connect(
     database = 'books',
 )
 
-## CRUD
-## comando = ''
-## cursor.execute(comando)
-## -> .commit(): edição do banco de dados
-## -> .fetchall(): leitura do banco 
-
 cursor = connection.cursor()
 
-app = Flask(__name__)
+cursor = connection.cursor(dictionary=True)
 
-books = [
-    {
-        'id': 1,
-        'title': 'Seu dinheiro pode mais',
-        'author': 'Gerson Costa'
-    },
-    {
-        'id': 2,
-        'title': 'Meu amigo Espírito Santo',
-        'author': 'PR Rodrigo Pena'
-    },
-    {
-        'id': 3,
-        'title': 'Até que nada mais importe',
-        'author': 'Luciano Subirá'
-    }
-]
+app = Flask(__name__)
 
 @app.route('/books', methods=['GET'])
 def get_books():
     command = 'SELECT * FROM books'
     cursor.execute(command)
     result = cursor.fetchall()
-    print(result)
-    return jsonify(books)
+    return jsonify(result)
 
 @app.route('/books/<int:id>', methods=['GET'])
 def get_book_by_id(id):
-    for book in books:
-        if book.get('id') == id:
-            return jsonify(book)
+    command = f'SELECT * FROM books WHERE id = {id}'
+    cursor.execute(command)
+    result = cursor.fetchall()
+    return jsonify(result)
 
 @app.route('/books/<int:id>', methods=['PUT'])
 def update_book_by_id(id):
     updated_book = request.get_json()
-    for index, book in enumerate(books):
-        if book.get('id') == id:
-            books[index].update(updated_book)
-            return jsonify(books[index])
+    command = f'UPDATE books SET author = "{updated_book["author"]}", title = "{updated_book["title"]}" WHERE id = {id}'
+    cursor.execute(command)
+    connection.commit()
 
 @app.route('/books', methods=['POST'])
 def create_book():
@@ -65,15 +41,14 @@ def create_book():
     command = f'INSERT INTO books (title, author) VALUES ("{new_book["title"]}", "{new_book["author"]}")'
     cursor.execute(command)
     connection.commit()
-    books.append(new_book)
-    return jsonify(books)
+    return get_books()
 
 @app.route('/books/<int:id>', methods=['DELETE'])
 def remove_book(id):
-    for index, book in enumerate(books):
-        if book.get('id') == id:
-            del books[index]
-    return jsonify(books)
+    command = f'DELETE FROM books WHERE id = {id}'
+    cursor.execute(command)
+    connection.commit()
+    return get_books()
 
 app.run(port=5000, host='localhost', debug=True)
 
