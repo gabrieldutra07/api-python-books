64c8b70064f328381194d39bd479deee57199a3a
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-12-21 12:27:27 -0300

    alteração na senha do banco

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import mysql.connector
 connection = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    password = 'admin',
+    password = 'admin1',
     database = 'books',
 )
 
